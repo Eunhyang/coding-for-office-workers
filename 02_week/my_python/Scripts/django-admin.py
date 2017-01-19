dcbdb821ad0ec0495fede48d204daa04513ba4b1
hyang <eunhyang90218@gmail.com>
2017-01-19 20:38:20 +0900

    Add 02_week started blog a

---
+++ 02_week/my_python/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hyang\dev\coding-for-office-workers\02_week\my_python\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
